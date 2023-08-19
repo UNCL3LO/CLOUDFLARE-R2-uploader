1efd34c97badd5b7c64fb9b7bdf20dfda0856212
UNCL3LO <COLLINSMUNENEMUTWIRI@GMAIL.COM>
2023-08-19 14:28:48 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cloudflare R2 Uploader</title>
+</head>
+<body>
+    <h1>Cloudflare R2 Uploader</h1>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Upload File" name="submit">
+    </form>
+</body>
+</html>
